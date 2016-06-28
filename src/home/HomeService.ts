bed89a14ead32322cda9b7e38e3e6a1116d5fa9c
Roman Roelofsen <romanroe@gmail.com>
2016-06-28 12:17:32 +0200

    Removed  RxJS demo

--- src/home/HomeService.ts
+++ src/home/HomeService.ts
@@ -1,17 +1,8 @@
 import {appModule} from "../app";
-import {Subject} from "rxjs/Subject";
 
 export class HomeService {
 
     private counter = 1;
-
-    constructor() {
-        const s = new Subject<number>();
-        s.subscribe(value => {
-            console.log("got:" + value);
-        });
-        s.next(1);
-    }
 
     getAndIncrement() {
         return this.counter++;
